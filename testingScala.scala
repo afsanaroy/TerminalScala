ef95b580940438b3edede9698496f90f917b34e5
Afsana B. Roy <AfsanaRoy@Afsanas-MacBook-Pro.local>
2020-01-28 17:16:40 +0530

    All Scalas

---
+++ testingScala.scala
@@ -0,0 +1,7 @@
+object MainObject {  
+   def main(args: Array[String]) {  
+      val result = checkIt(-10)  
+      println (result)  
+   }  
+    def checkIt (a:Int)  =  if (a >= 0) 1 else -1    // Passing a if expression value to function  
+}  
